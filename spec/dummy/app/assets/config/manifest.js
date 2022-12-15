a374be25f1238a97581108172d4ae96306b07344
captproton <carl@wdwhub.net>
2022-12-15 14:11:58 -0800

    Adding rails 7 gems

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -1,3 +1,3 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
 //= link platform_cms_manifest.js
+//= link_tree ../builds
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'dummy_rails7_testing/index'
   mount PlatformCms::Engine => "/platform_cms"
 end

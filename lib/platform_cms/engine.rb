c3b7ccd4090cfb0cad411d8c5fee66a0dd3a09b2
captproton <carl@wdwhub.net>
2022-12-15 14:08:35 -0800

    Installed rspec

--- lib/platform_cms/engine.rb
+++ lib/platform_cms/engine.rb
@@ -1,5 +1,13 @@
 module PlatformCms
   class Engine < ::Rails::Engine
     isolate_namespace PlatformCms
+
+    config.generators do |g|
+      g.test_framework :rspec, fixtures: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
+
   end
 end
